2a437a08ae51ac56fd640f0dfd9f8a8fdfc8dd71
unknown <petar.korudzhiev@pKorudzhiev-PC.ad.mentormate.bg>
2016-05-27 19:33:28 +0300

    Initial commit

---
+++ MongoTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("restaurants");
+
+            // Test first
+            /* 
+            var aggregate = collection.Aggregate().Group(new BsonDocument { { "_id", "$borough" }, { "count", new BsonDocument("$sum", 1) } });
+            var results = await aggregate.ToListAsync();
+            */
+
+
+            // Second test
+            
+            var aggregate = collection.Aggregate()
+                .Match(new BsonDocument { { "borough", "Queens" }, { "cuisine", "Brazilian" } })
+                .Group(new BsonDocument { { "_id", "$address.zipcode" }, { "count", new BsonDocument("$sum", 1) } });
+            var results = await aggregate.ToListAsync();
+
+            foreach(var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
