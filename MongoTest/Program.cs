731f3aae5e723455f61c4c4abf7c53f3c8a851b3
Petar Korudzhiev <Petar Korudzhiev>
2016-05-28 09:36:26 +0300

    Using Fluent Api

--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,9 @@ namespace MongoTest
             var client = new MongoClient(connectionString);
 
             var db = client.GetDatabase("test");
-            var collection = db.GetCollection<BsonDocument>("restaurants");
+            var collection = db.GetCollection<Restaurant>("restaurants");
+
+            //Builders<Restaurant>.Filter.Where(x => x.Borough == "Queens")
 
             // Test first
             /* 
@@ -33,7 +36,7 @@ namespace MongoTest
 
 
             // Second test
-            
+            /*
             var aggregate = collection.Aggregate()
                 .Match(new BsonDocument { { "borough", "Queens" }, { "cuisine", "Brazilian" } })
                 .Group(new BsonDocument { { "_id", "$address.zipcode" }, { "count", new BsonDocument("$sum", 1) } });
@@ -42,7 +45,56 @@ namespace MongoTest
             foreach(var result in results)
             {
                 Console.WriteLine(result);
+            }*/
+
+            
+            var lll = await collection.Aggregate()
+                .Match(x => x.Borough == "Queens" && x.Cuisine == "Brazilian")
+                .Group(i => i.Address.ZipCode, g => new { Count = g.Count() }).ToListAsync();
+
+            foreach (var result in lll)
+            {
+                Console.WriteLine(result);
             }
         }
+    }
+
+    public class Restaurant
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("borough")]
+        public string Borough { get; set; }
+        [BsonElement("cuisine")]
+        public string Cuisine { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("restaurant_id")]
+        public string RestaurantId { get; set; }
+        [BsonElement("address")]
+        public Address Address { get; set; }
+        [BsonElement("grades")]
+        public List<Grade> Grades { get; set; }
+    }
+
+    public class Address
+    {
+        [BsonElement("building")]
+        public string Building { get; set; }
+        [BsonElement("coord")]
+        public List<double> Coord { get; set; }
+        [BsonElement("street")]
+        public string Street { get; set; }
+        [BsonElement("zipcode")]
+        public string ZipCode { get; set; }
+    }
+
+    public class Grade
+    {
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+        [BsonElement("grade")]
+        public string GradeName { get; set; }
+        [BsonElement("score")]
+        public double? Score { get; set; }
     }
 }
